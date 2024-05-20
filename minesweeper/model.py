6fb4e6e885c01e769b8b19a57a42191a1359ca32
ПК <petrenkoartem2001@gmail.com>
2024-05-20 17:49:17 +0300

    add_game level

--- minesweeper/model.py
+++ minesweeper/model.py
@@ -31,3 +31,50 @@ class Model:
 
 	"""TODO: Write test to find out when mines amounts
 	   greater then spots."""
+
+	def empty_func(self):
+		"""When player did not change game level
+		   and we want play last one."""
+		pass
+
+	def new_game_junior(self):
+		self.FIELD_WIDTH = 10
+		self.FIELD_HEIGHT = 10
+		self.MINES_MAX = 10
+
+	def new_game_middle(self):
+		self.FIELD_WIDTH = 14
+		self.FIELD_HEIGHT = 6
+		self.MINES_MAX = 15
+
+	def new_game_senior(self):
+		self.FIELD_WIDTH = 15
+		self.FIELD_HEIGHT = 15
+		self.MINES_MAX = 25
+
+	def create_field(self):
+		# Creating field.
+		self.first_click = True
+		self.seconds_from_start = 1
+		self.flagged_cells = 0
+		self.open_cells = 0
+		self.must_open_cells = (self.FIELD_WIDTH * self.FIELD_HEIGHT
+								- self.MINES_MAX)
+		self.is_game_over = False
+		self.stop_game = False
+		self.field = []
+		for y in range(self.FIELD_HEIGHT):
+			_xrow = []
+			for x in range(self.FIELD_WIDTH):
+				cell = Cell(x, y)
+				_xrow.append(cell)
+			self.field.append(_xrow)
+
+		# Setting mines at field.
+		mines_ammout = 0
+		while mines_ammout < self.MINES_MAX:
+			_x = randint(0, self.FIELD_WIDTH - 1)
+			_y = randint(0, self.FIELD_HEIGHT - 1)
+			if not self.field[_y][_x].mined:
+				mines_ammout += 1
+				self.field[_y][_x].mined = True
